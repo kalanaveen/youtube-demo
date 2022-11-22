b547a486e93472219ba94616fe2b09361d698944
naveen kala <kalanaveen55@gmail.com>
2022-11-22 20:04:11 +0530

    navbar created

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { IconButton, Paper } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+  return (
+    <Paper
+      component="form"
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton
+        sx={{ p: '10px', color: 'red' }}
+        aria-label="search"
+        type="submit"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
