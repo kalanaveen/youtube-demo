925c3c6f85c725b513bb910c9d87664bd47cde8c
naveen kala <kalanaveen55@gmail.com>
2022-11-23 16:49:36 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ const App = () => {
       <Box sx={{ backgroundColor: '#000' }}>
         <Navbar />
         <Routes>
+          <Route exact path="/" element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
           <Route path="/channel/:id" element={<ChannelDetail />} />
           <Route path="/search/:searchTerm" element={<SearchFeed />} />
-          <Route exact path="/" element={<Feed />} />
         </Routes>
       </Box>
     </BrowserRouter>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import Videos from './Videos';
 
 const Feed = () => {
-  return (
-    <div>
-      feed
-    </div>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState('New');
 
-export default Feed
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright © 2023 KALA Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          <span style={{ color: '#FC1503' }}>Videos</span>
+        </Typography>
+        <Videos />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
