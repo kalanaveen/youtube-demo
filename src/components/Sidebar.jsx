0e5237b4ab6d4baee9e2c794744fa99f2009a9cf
naveen kala <kalanaveen55@gmail.com>
2022-11-28 17:20:04 +0530

    final updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
             color: 'white',
           }}
           onClick={() => setSelectedCategory(category.name)}
-          key = {category.name}
+          key={category.name}
         >
           <span
             style={{
